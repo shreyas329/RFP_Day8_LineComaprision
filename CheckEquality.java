e1121b717b67e0694ed4f6dd2db55964f1b1379a
shreyas329 <sharmayashu1301@gmail.com>
2022-09-21 22:02:13 +0530

    Check the equality of two lengths.

---
+++ CheckEquality.java
@@ -0,0 +1,15 @@
+package bridgelabz;
+
+public class CheckEquality {
+
+	public static void main(String[] args) {
+		double  x1 = 8, y1 = 19;
+        double  x2 = 15, y2 = 25;
+
+        double value = ((Math.sqrt (x2 - x1) * (x2 -x1)) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of line is :" +value);
+
+
+	}
+
+}
